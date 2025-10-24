28b4271429f19dec9d160b3283b948b2f9f2f1e4
Ilan Laloum <isramail@gmail.com>
2025-10-24 09:19:33 +0300

    refactor: Restructure EntityViewModel tests to improve clarity and organization

--- src/components/views/EntityViewModel.test.js
+++ src/components/views/EntityViewModel.test.js
@@ -4,7 +4,12 @@ const { EMD } = require('../../config/EMD');
 const EntityViewModel = require('./EntityViewModel');
 
 describe('EntityViewModel', () => {
-    it('should create an instance using the static create method', () => {
+    const tests = [
+        EMD.Bot,
+        EMD.Home
+    ];
+
+    it('should create an view model instance.', () => {
         const viewModel = EntityViewModel.create({
             cardService: CardService,
             activeSpreadsheet: SpreadsheetApp.getActiveSpreadsheet()
@@ -12,41 +17,41 @@ describe('EntityViewModel', () => {
         expect(viewModel).toBeDefined();
     });
 
-    // getCardBuilder
-    it('should create a card from the cardMeta', () => {
-        const cardMeta = EMD.Bot.cardMeta;
+    describe('EntityViewModel', () => {
         const viewModel = EntityViewModel.create({
             cardService: CardService,
             activeSpreadsheet: SpreadsheetApp.getActiveSpreadsheet()
         });
-        expect(viewModel).toBeDefined();
-        const card = viewModel.getCardBuilder(cardMeta);
-        expect(card).toBeDefined();
-        const builtCard = card.build();
-        const data = builtCard.getData();
-        expect(data).toBeDefined();
-        expect(data.name).toBe(cardMeta.name);
-        expect(data.header).toBeDefined();
-        expect(data.header.title).toBe(cardMeta.header.title);
-        expect(data.header.subTitle).toBe(cardMeta.header.subTitle);
-        expect(data.header.imageUrl).toBe(cardMeta.header.imageUrl);
-        expect(data.header.imageStyle).toBe(cardMeta.header.imageStyle);
-        expect(data.header.imageAltText).toBe(cardMeta.header.imageAltText);
-        expect(data.sections).toBeDefined();
-        expect(data.sections.length).toBe(cardMeta.sections.length);
+
+        tests.forEach((emd) => {
+            describe(`Testing entity: ${emd.entityName}`, () => {                
+                it('should create a card from the "Bot" cardMeta', () => {
+                    const cardMeta = emd.cardMeta;
+                    const card = viewModel.getCardBuilder(cardMeta);
+                    expect(card).toBeDefined();
+                    const builtCard = card.build();
+                    const data = builtCard.getData();
+                    expect(data).toBeDefined();
+                    expect(data.name).toBe(cardMeta.name);
+                    expect(data.header).toBeDefined();
+                    expect(data.header.title).toBe(cardMeta.header.title);
+                    expect(data.header.subTitle).toBe(cardMeta.header.subTitle);
+                    expect(data.header.imageUrl).toBe(cardMeta.header.imageUrl);
+                    expect(data.header.imageStyle).toBe(cardMeta.header.imageStyle);
+                    expect(data.header.imageAltText).toBe(cardMeta.header.imageAltText);
+                    expect(data.sections).toBeDefined();
+                    expect(data.sections.length).toBe(cardMeta.sections.length);
+                });
+
+                it('should get the active sheet', () => {
+                    const sheetMeta = emd.sheetMeta;
+                    if(!sheetMeta) return;
+                    const activeSheet = viewModel.getActiveSheet(sheetMeta);
+                    expect(activeSheet).toBeDefined();
+                });
+
+            });
+        });
     });
 
-    // getActiveSheet
-    it('should get the active sheet', () => {
-        const sheetMeta = {
-            name: 'TestSheet',
-            columns: ['Column1', 'Column2', 'Column3']
-        };
-        const viewModel = EntityViewModel.create({
-            cardService: CardService,
-            activeSpreadsheet: SpreadsheetApp.getActiveSpreadsheet()
-        });
-        const activeSheet = viewModel.getActiveSheet(sheetMeta);
-        expect(activeSheet).toBeDefined();
-    });
 });
