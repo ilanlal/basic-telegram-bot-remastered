52380c9495bc6a1ba479af8a5726ae6514853ef9
Ilan Laloum <isramail@gmail.com>
2025-11-14 01:02:22 +0200

    refactor: remove AccountController and its tests; update index.js to reflect changes

--- src/components/controllers/index.js
+++ src/components/controllers/index.js
@@ -1,8 +1,5 @@
 const { BotSetupController } = require('./BotSetupController');
-const { AccountController, AccountControllerFactory } = require('./AccountController');
 const { EntityController } = require('./EntityController');
 
 global.EntityController = EntityController;
 global.BotSetupController = BotSetupController;
-global.AccountControllerFactory = AccountControllerFactory;
-global.AccountController = AccountController;
